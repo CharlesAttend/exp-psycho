30a79ed36fcb52aa4a6d96ce9ad9b5e0a998ed71
Charles Attend <charles.vin@outlook.fr>
2022-03-05 21:20:53 +0100

    working

--- src/App.jsx
+++ src/App.jsx
@@ -5,34 +5,34 @@ import Question from './questionBank'
 function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
 
-  const [startingMood, setStartingMood] = useState("")
+  const [survey, setSurvey] = useState("")
   const [RTList, setRTList] = useState([]);
-  const [totalQuestion, setTotalQuestion] = useState(6);
-  const [emotion, setEmotion] = useState(choose(['sad', 'neutral']))
+  const [totalQuestion, _setTotalQuestion] = useState(8);
+  const [emotion, _setEmotion] = useState(choose(['sad', 'neutral']))
   console.log(emotion);
 
-  const handleAnswerButtonClick = (answerOption) => {
-    if(currentQuestion===totalQuestion){
+  const handleSubmitButtonClick = () => {
       //do web request
       const requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ reaction_time: RTList, emotion: emotion, startingMood: startingMood})
+        body: JSON.stringify({ survey: survey, emotion: emotion, reaction_time: RTList })
       };
       fetch('https://621bb200768a4e10209a9b6e.mockapi.io/result', requestOptions)
       .then(response => response.json())
       .then(data => console.log("worked", data));
-      return 0;
-    }
-    const nextQuestion = currentQuestion + 1;
-    setCurrentQuestion(nextQuestion);
   };
   
   return (
     <div className="App">
       <div>Question {currentQuestion} sur {totalQuestion}</div>
-      <Question className="h-10" id={currentQuestion} onRTUpdate={setRTList} emotion={emotion} onMoodUpdate={setStartingMood}/>
-      <button className="text-xl" onClick={() => handleAnswerButtonClick()}>Question Suivante</button>
+      <Question className="h-10" id={currentQuestion} 
+        onRTUpdate={setRTList}
+        emotion={emotion}
+        setSurvey={setSurvey}
+        setCurrentQuestion={setCurrentQuestion}
+        handleSubmitButtonClick={handleSubmitButtonClick}
+      />
     </div>
   );
 }
--- src/questionBank.jsx
+++ src/questionBank.jsx
@@ -1,15 +1,17 @@
 import { useState, useEffect } from "react";
 import { useForm } from'react-hook-form';
 
-const Question = ({id, onRTUpdate, emotion, onMoodUpdate}) => {
+const Question = ({id,setCurrentQuestion, onRTUpdate, emotion, setSurvey, handleSubmitButtonClick}) => {
     const QuestionList = [
-        Question0(),
-        Question1(onMoodUpdate),
-        Question2(),
-        Question3(emotion), 
-        Question4(), 
-        Question5(onRTUpdate), 
-        Question6];
+        Accueil(setCurrentQuestion),
+        Survey(setCurrentQuestion,setSurvey),
+        ConsigneVideo(setCurrentQuestion, emotion),
+        QuestionVideo(setCurrentQuestion,emotion), 
+        SurveyVideo(setCurrentQuestion,emotion), 
+        ConsigneRTTest(setCurrentQuestion),
+        TrialRTTest(setCurrentQuestion),
+        QuestionRTTest(setCurrentQuestion,onRTUpdate), 
+        FinalQuestion(setCurrentQuestion, handleSubmitButtonClick)];
     return (
         <div>
             {QuestionList[id]}
@@ -17,58 +19,113 @@ const Question = ({id, onRTUpdate, emotion, onMoodUpdate}) => {
     )
 }
 
-const Question0 = () => (
+const Accueil = (setCurrentQuestion) => (
     <div>
-        <div>Merci de participer à mon expérience ! Lisez bien les consignes à chaque étapes.</div>
+        <div>Merci de participer à mon expérience ! Lisez bien les consignes à chaque étapes et prenez votre temps.</div>
         <div>Le tout prends moins de dix minutes, l'expérience est à faire seule</div>
         <div>N'actualisé pas la page, vous ne pouvez pas retourner en arrière</div>
+        <NextQuestionButton setCurrentQuestion={setCurrentQuestion}/>
     </div>
 )
 
-const Question1 = (onMoodUpdate) => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
-
+const Survey = (setCurrentQuestion, setSurvey) => {
+    const { register, handleSubmit } = useForm({
+        defaultValues: {
+            firstName: "",
+            currentMood: 7
+        }
+    });
+    const onSubmit = data => {
+        console.log(data);
+        setSurvey(data); 
+        setCurrentQuestion((prevState) => prevState+1)
+    };
+    
     return (
         <div>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <label>
                     Votre prénom:
-                    <input type="text" defaultValue="Prénom" {...register('firstName', { required: true })} />
+                    <input type="text" placeholder="Prénom" {...register('firstName', { required: true })} />
                 </label>
-                <label htmlFor="">
+                <label>
                     Votre mood actuel
-                    <input type="range" name="name" {...register('mood', { required: true, min: 0, max: 10 })}></input>
+                    <select {...register("currentMood", { required: true })}>
+                        {[0,1,2,3,4,5,6,7,8,9,10].map((nb, id)=> 
+                            <option value={nb.toString()} key={id}>{nb}</option>
+                        )}
+                    </select>
                 </label>
-                {errors.exampleRequired && <span>This field is required</span>}
-                <input type="submit"/>
+                <input type="submit" value="Valider 👌"/>
             </form>
         </div>
     )
 }
 
-const Question2 = () => (
-    <div>
-        <div>Vous allez visionner une courte video</div>
-        <div>Restez attentif tout au long de la vidéo</div>
-    </div>
-)
-
-const Question3 = (emotion) => {
-    const sadVid = <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/oxfwLIKTyFk?controls=0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-    const neutralVid = <div>neutral vid</div>
-    return (<div>
-        {emotion==="sad" ? sadVid : neutralVid} 
-    </div>)
-}
-
-const Question4 = () =>{
+const ConsigneVideo = (setCurrentQuestion, emotion) => {
+    const sadVid = <div>Un boxeur est allongé grièvement blessé sur une table, lorsque son petit fils entre.</div>
+    const neutralVid = <div>Hannah et Holly font du shopping. Elles parlent d'hier soir.</div>
     return (
-        <div>jes suis une fct</div>
+        <div>
+            <div>Vous allez visionner une courte video, en voici le résumé :</div>
+            <div>{emotion==="sad" ? sadVid : neutralVid}</div>
+
+            <div>Restez attentif tout au long de celle-ci.</div>
+            <NextQuestionButton setCurrentQuestion={setCurrentQuestion}/>
+        </div>
     )
 }
 
-const Question5 = (onRTUpdate) => {
+const QuestionVideo = (setCurrentQuestion, emotion) => {
+    const sadVid = <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/oxfwLIKTyFk?controls=0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+    const neutralVid = <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/Jwm4DPCje1U?controls=0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+    return (
+        <div>
+            <div>{emotion==="sad" ? sadVid : neutralVid}</div>
+            <NextQuestionButton setCurrentQuestion={setCurrentQuestion}/>
+        </div>
+    )
+}
+
+const SurveyVideo = (setCurrentQuestion, emotion) =>{
+    return (
+        <div>
+        <label>
+            Qu'avez vous pensez de la vidéo ? Notez la entre 0 et 10.
+            <input type="number"/>
+        </label>
+        <NextQuestionButton setCurrentQuestion={setCurrentQuestion}/>
+    </div>
+    )
+}
+
+const ConsigneRTTest = (setCurrentQuestion) => (
+    <div>
+        <div>Vous allez faire un test de temps de réaction</div>
+        <div>Lorsque l'arrière plan change de couleur, cliquez n'importe où le plus vite possible</div>
+        <div>La prochaine question sera un entraînement d'un essai</div>
+        <NextQuestionButton setCurrentQuestion={setCurrentQuestion}/>
+    </div>
+)
+const TrialRTTest = (setCurrentQuestion) => {
+    const [RT, setRT] = useState(0);
+    const [isFinish, setIsFinish] = useState(false)
+    const cl = (RT) => {
+        setIsFinish(true);
+        setRT(RT);
+    }
+    const testEndedDiv = <div>
+            <div>Votre temps de réaction : {RT}</div>
+            <div>Prêt pour le vrais test ? Passez à la question suivante.</div>
+            <NextQuestionButton setCurrentQuestion={setCurrentQuestion}/>
+        </div>
+    return (
+        <div>
+            {isFinish ? testEndedDiv : <ReactionTimeTest callback={cl}/>}
+        </div>
+    )
+}
+const QuestionRTTest = (setCurrentQuestion, onRTUpdate) => {
     const [nbEssai, setNbEssai] = useState(0);
     const cb = (RT) => {
         setNbEssai(prevState => prevState+1);
@@ -76,18 +133,23 @@ const Question5 = (onRTUpdate) => {
             prevState.push(RT)
             return prevState
         });
-        console.log("Callback:", RT);
+        console.log("RT:", RT);
     }
-    if (nbEssai<5) {
+    if (nbEssai<50) {
         return <div><ReactionTimeTest key={nbEssai} callback={cb}/></div>
     }
     else {
-        return <div>Test terminé, allez à la question suivante ;)</div>
+        return (
+            <div>
+                <div>Test terminé, allez à la question suivante ;)</div>
+                <NextQuestionButton setCurrentQuestion={setCurrentQuestion}/>
+            </div>
+        )
     }
 };
 
 const ReactionTimeTest = ({callback}) => {
-    const [compteur, setCompteur] = useState(3);
+    const [compteur, setCompteur] = useState(0);
     const [reaction, setReaction] = useState(false);
     const [timeBefore, setTimeBefore] = useState(0);
     const [intervalID, setIntervalID] = useState(0);
@@ -105,32 +167,46 @@ const ReactionTimeTest = ({callback}) => {
 
     useEffect(() =>{
         if (compteur===0){
-            console.log(intervalID);
             clearInterval(intervalID)
             setIsStarted(true);
 
             const timeToWait = getRandomInt(1000, 3000);
-            setTimeBefore(Date.now());
-            setTimeout(() => {
+            // setTimeBefore(Date.now());
+            const timeoutIntervalID = setTimeout(() => {
+                setTimeBefore(performance.now());
                 setReaction(true);
             }, timeToWait);
+            console.log("TimeBefore", timeBefore, "timeToWait", timeToWait);
+            return () => clearInterval(timeoutIntervalID)
         }
     }, [compteur, intervalID])
 
     return (
         <div>
             {!isStarted && (<div>{compteur}</div>)}
-            <div className={reaction ? "h-full bg-red-600" : "h-full bg-gray-400"} onClick={reaction ? ((e) => {callback(Date.now() - timeBefore);}) : undefined }>
+            <div className={reaction ? "h-full bg-red-600" : "h-full bg-gray-400"} onClick={reaction ? ((e) => {callback(performance.now() - timeBefore);}) : undefined }>
                 test
             </div>
         </div>
     )
 }
 
-const Question6 = () => (
+const FinalQuestion = (setCurrentQuestion, handleSubmitButtonClick) => (
     <div>
-        <div>Félicitation pd</div>
+        <div>C'est terminé, merci beaucoup de votre participation !</div>
+        <div>Vous pouvez envoyer vos résultats en cliquant sur le bouton</div>
+        <button className="text-xl" 
+        onClick={(e) => handleSubmitButtonClick()}>
+            Envoyer mes résultats ! 
+        </button>
     </div>
+)
+
+const NextQuestionButton = ({setCurrentQuestion}) => (
+    <button className="text-xl" 
+        onClick={(e) => setCurrentQuestion((prevState) => prevState + 1)}>
+            Question Suivante
+    </button>
 )
 
 function getRandomInt(min, max) {
